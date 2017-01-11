3ad0769080c7dc1e1c921dc487b6d4b775b3712e
Caitlin Hines and grinil khanna <grinil@>
2017-01-11 14:37:22 -0800

    Working jquery calculator with arithmitic functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,61 @@
+// Business (or back-end) logic:
+
 var add = function(number1,number2){
 	return number1 + number2;
 };
+var subtract = function(number1,number2) {
+	return number1 - number2;
+};
+var multiply = function(number1,number2) {
+	return number1 * number2;
+};
+var divide = function(number1,number2) {
+	return number1 / number2;
+};
+// Everything below this line is user interface (or front-end) logic:
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+	var number1 = parseInt($("#add1").val());
+	var number2 = parseInt($("#add2").val());
+	var result = (add(number1, number2));
+	$("#addOutput").text(result);
+	});
+});
 
-alert(add(number1, number2));
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+	var number1 = parseInt($("#subtract1").val());
+	var number2 = parseInt($("#subtract2").val());
+	var result = (subtract(number1, number2));
+	$("#subOutput").text(result);
+	});
+});
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+	var number1 = parseInt($("#multiply1").val());
+	var number2 = parseInt($("#multiply2").val());
+	var result = (multiply(number1, number2));
+	$("#multiOutput").text(result);
+	});
+});
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+	var number1 = parseInt($("#divide1").val());
+	var number2 = parseInt($("#divide2").val());
+	var result = (divide(number1, number2));
+	$("#divideOutput").text(result);
+	});
+});
